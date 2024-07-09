d0559a2373470610e6980eb9d88b48aac63c1990
xlxdd <393025885@qq.com>
2024-07-09 12:06:55 +0800

    创建看门狗

---
+++ ProcessManager/Services&Interfaces/WatchDog/Dog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ProcessManager.Services_Interfaces.WatchDog;
+/// <summary>
+/// 来源
+/// https://www.cnblogs.com/chonglu/p/16913746.html
+/// </summary>
+public class Dog
+{
+    private readonly DispatcherTimer timer = new ();
+    public Dog(int interval = 1000)
+    {
+        
+    }
+}
+
